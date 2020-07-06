3868099514495e1b17fe8e266e5e99f075c8b983
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-07-06 18:00:57 +0100

    Adds cors

--- Patientor_backend/index.ts
+++ Patientor_backend/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 const app = express();
+import cors from "cors";
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.use(cors());
+
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
-  res.send("pong");
+  res.json({ hello: "hello" });
 });
 
 app.listen(PORT, () => {
