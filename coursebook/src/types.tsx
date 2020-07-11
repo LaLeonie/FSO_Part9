ca08a1e8d882c9fb088313968f888ab349c2fab4
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-07-11 12:16:57 +0100

    Adds new types for courseParts

--- coursebook/src/types.tsx
+++ coursebook/src/types.tsx
@@ -10,3 +10,28 @@ export interface Course {
 export interface ContentProps {
   parts: Array<Course>;
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface ExtendedCoursePartBase extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends ExtendedCoursePartBase {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends ExtendedCoursePartBase {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
