48573afad76d6264fc90843b1697ef4b0d3860b1
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2020-07-15 09:35:14 +0100

    Extends types

--- Patientor_frontend/src/types.ts
+++ Patientor_frontend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnosis {
+export interface DiagnoseEntry {
   code: string;
   name: string;
   latin?: string;
@@ -7,7 +7,7 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
 export interface Patient {
@@ -17,4 +17,52 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
+
+interface DischargeEntry {
+  date: string;
+  criteria: string;
+}
+
+interface SickLeaveEntry {
+  startDate: string;
+  endDate: string;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<DiagnoseEntry["code"]>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: DischargeEntry;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeaveEntry;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
